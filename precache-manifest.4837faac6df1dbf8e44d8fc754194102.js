da0f0163bfda19ab882c9fde8dbd37a645cc256f
toxuh <zakharov.toxa@gmail.com>
2020-01-17 19:03:54 +0100

    Updates

---
+++ precache-manifest.4837faac6df1dbf8e44d8fc754194102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c559025c89b92994bdfe2e6581c41b",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ff088491d8219de00941",
+    "url": "/tic-tac-toe/static/css/main.e14e618d.chunk.css"
+  },
+  {
+    "revision": "1e2909bb57981d02d6ef",
+    "url": "/tic-tac-toe/static/js/2.72aa86e2.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/tic-tac-toe/static/js/2.72aa86e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff088491d8219de00941",
+    "url": "/tic-tac-toe/static/js/main.8d0504d9.chunk.js"
+  },
+  {
+    "revision": "37b1a36cd7995ee344a0",
+    "url": "/tic-tac-toe/static/js/runtime-main.7549cc49.js"
+  }
+]);
